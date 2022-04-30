3f8f56f166fe7eef7d536b53a05808a8bb840898
zach <zacharybuchli@gmail.com>
2022-04-30 15:18:59 -0500

    added hero component start

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,9 +1,57 @@
-
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 export default function Hero() {
     return (
-        <>
-            <div>Hero Section</div>
-        </>
-    )
+        <div className="relative bg-inherit overflow-hidden">
+            <div className="max-w-7xl mx-auto">
+                <div className="relative z-10 pb-8 bg-inherit sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+                    <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                        <div className="sm:text-center lg:text-left">
+                            <h1 className="text-4xl tracking-tight font-extrabold sm:text-5xl md:text-6xl">
+                                <span className="block text-blue-500">
+                                    Zach Buchli
+                                </span>{" "}
+                                <span className="block text-black ">
+                                    Software Developer
+                                </span>
+                            </h1>
+                            <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                                Passionate Software Developer with experience
+                                deliverying valuable software solutions that
+                                scale.
+                            </p>
+                            <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                                <div className="rounded-md shadow">
+                                    <a
+                                        href="#projects"
+                                        className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-700 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
+                                    >
+                                        Projects
+                                    </a>
+                                </div>
+                                <div className="mt-3 sm:mt-0 sm:ml-3">
+                                    <a
+                                        href="#contact"
+                                        className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-indigo-100 hover:bg-blue-200 md:py-4 md:text-lg md:px-10"
+                                    >
+                                        Contact
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </main>
+                </div>
+            </div>
+            <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+                <img
+                    className="h-56 w-100 object-cover sm:h-72 md:h-96"
+                    src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+                    alt=""
+                />
+            </div>
+        </div>
+    );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,20 @@
-import Layout from '../components/layout'
-import About from '../components/about'
-import Projects from '../components/projects'
-import Contact from '../components/contact'
-import Hero from '../components/hero'
-
-
+import Layout from "../components/layout";
+import About from "../components/about";
+import Projects from "../components/projects";
+import Contact from "../components/contact";
+import Hero from "../components/hero";
 
 export default function Home() {
-  return (
-    <>
-      <Layout>
-        <div className="flex flex-col items-center justify-center">
-          <Hero />
-          <About />
-          <Projects />
-          <Contact />
-        </div>
-      </Layout>
-    </>
-  )
+    return (
+        <>
+            <Layout>
+                <div className="flex flex-col justify-center">
+                    <Hero />
+                    <About />
+                    <Projects />
+                    <Contact />
+                </div>
+            </Layout>
+        </>
+    );
 }
